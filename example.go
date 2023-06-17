e3ff29b402bded04a33fb89b704647c3bb608a19
d-smith <doug.smith.mail@gmail.com>
2023-06-17 07:34:08 -0700

    Streamlined example, added more output context

--- example.go
+++ example.go
@@ -12,26 +12,11 @@ import (
 	"github.com/taurusgroup/multi-party-sig/pkg/party"
 	"github.com/taurusgroup/multi-party-sig/pkg/pool"
 	"github.com/taurusgroup/multi-party-sig/pkg/protocol"
-	"github.com/taurusgroup/multi-party-sig/pkg/taproot"
 	"github.com/taurusgroup/multi-party-sig/protocols/cmp"
-	"github.com/taurusgroup/multi-party-sig/protocols/example"
-	"github.com/taurusgroup/multi-party-sig/protocols/frost"
 )
 
-func XOR(id party.ID, ids party.IDSlice, n *test.Network) error {
-	h, err := protocol.NewMultiHandler(example.StartXOR(id, ids), nil)
-	if err != nil {
-		return err
-	}
-	test.HandlerLoop(id, h, n)
-	_, err = h.Result()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func CMPKeygen(id party.ID, ids party.IDSlice, threshold int, n *test.Network, pl *pool.Pool) (*cmp.Config, error) {
+	fmt.Println("running key gen for ", id)
 	h, err := protocol.NewMultiHandler(cmp.Keygen(curve.Secp256k1{}, id, ids, threshold, pl), nil)
 	if err != nil {
 		return nil, err
@@ -74,123 +59,15 @@ func CMPSign(c *cmp.Config, m []byte, signers party.IDSlice, n *test.Network, pl
 
 	signature := signResult.(*ecdsa.Signature)
 	sigBytes, err := signature.SigEthereum()
-	fmt.Println("signature be ", base64.StdEncoding.EncodeToString(sigBytes))
+	fmt.Println("eth sig from ", c.ID, " is ", base64.StdEncoding.EncodeToString(sigBytes))
 	if !signature.Verify(c.PublicPoint(), m) {
 		return errors.New("failed to verify cmp signature")
-	}
-	return nil
-}
-
-func CMPPreSign(c *cmp.Config, signers party.IDSlice, n *test.Network, pl *pool.Pool) (*ecdsa.PreSignature, error) {
-	h, err := protocol.NewMultiHandler(cmp.Presign(c, signers, pl), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	test.HandlerLoop(c.ID, h, n)
-
-	signResult, err := h.Result()
-	if err != nil {
-		return nil, err
-	}
-
-	preSignature := signResult.(*ecdsa.PreSignature)
-	if err = preSignature.Validate(); err != nil {
-		return nil, errors.New("failed to verify cmp presignature")
-	}
-	return preSignature, nil
-}
-
-func CMPPreSignOnline(c *cmp.Config, preSignature *ecdsa.PreSignature, m []byte, n *test.Network, pl *pool.Pool) error {
-	h, err := protocol.NewMultiHandler(cmp.PresignOnline(c, preSignature, m, pl), nil)
-	if err != nil {
-		return err
-	}
-	test.HandlerLoop(c.ID, h, n)
-
-	signResult, err := h.Result()
-	if err != nil {
-		return err
-	}
-	signature := signResult.(*ecdsa.Signature)
-	if !signature.Verify(c.PublicPoint(), m) {
-		return errors.New("failed to verify cmp signature")
-	}
-	return nil
-}
-
-func FrostKeygen(id party.ID, ids party.IDSlice, threshold int, n *test.Network) (*frost.Config, error) {
-	h, err := protocol.NewMultiHandler(frost.Keygen(curve.Secp256k1{}, id, ids, threshold), nil)
-	if err != nil {
-		return nil, err
-	}
-	test.HandlerLoop(id, h, n)
-	r, err := h.Result()
-	if err != nil {
-		return nil, err
-	}
-
-	return r.(*frost.Config), nil
-}
-
-func FrostSign(c *frost.Config, id party.ID, m []byte, signers party.IDSlice, n *test.Network) error {
-	h, err := protocol.NewMultiHandler(frost.Sign(c, signers, m), nil)
-	if err != nil {
-		return err
-	}
-	test.HandlerLoop(id, h, n)
-	r, err := h.Result()
-	if err != nil {
-		return err
-	}
-
-	signature := r.(frost.Signature)
-	if !signature.Verify(c.PublicKey, m) {
-		return errors.New("failed to verify frost signature")
-	}
-	return nil
-}
-
-func FrostKeygenTaproot(id party.ID, ids party.IDSlice, threshold int, n *test.Network) (*frost.TaprootConfig, error) {
-	h, err := protocol.NewMultiHandler(frost.KeygenTaproot(id, ids, threshold), nil)
-	if err != nil {
-		return nil, err
-	}
-	test.HandlerLoop(id, h, n)
-	r, err := h.Result()
-	if err != nil {
-		return nil, err
-	}
-
-	return r.(*frost.TaprootConfig), nil
-}
-func FrostSignTaproot(c *frost.TaprootConfig, id party.ID, m []byte, signers party.IDSlice, n *test.Network) error {
-	h, err := protocol.NewMultiHandler(frost.SignTaproot(c, signers, m), nil)
-	if err != nil {
-		return err
-	}
-	test.HandlerLoop(id, h, n)
-	r, err := h.Result()
-	if err != nil {
-		return err
-	}
-
-	signature := r.(taproot.Signature)
-	if !c.PublicKey.Verify(signature, m) {
-		return errors.New("failed to verify frost signature")
 	}
 	return nil
 }
 
 func All(id party.ID, ids party.IDSlice, threshold int, message []byte, n *test.Network, wg *sync.WaitGroup, pl *pool.Pool) error {
 	defer wg.Done()
-
-	// XOR
-	//fmt.Println("xor")
-	//err := XOR(id, ids, n)
-	//if err != nil {
-	//	return err
-	//}
 
 	// CMP KEYGEN
 	fmt.Println("cmp keygen")
@@ -200,25 +77,13 @@ func All(id party.ID, ids party.IDSlice, threshold int, message []byte, n *test.
 	}
 
 	// CMP REFRESH
-	fmt.Println("cmp refresh")
-	refreshConfig, err := CMPRefresh(keygenConfig, n, pl)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("%+v\n", refreshConfig)
-
-	// FROST KEYGEN
-	//frostResult, err := FrostKeygen(id, ids, threshold, n)
+	//fmt.Println("cmp refresh")
+	//refreshConfig, err := CMPRefresh(keygenConfig, n, pl)
 	//if err != nil {
 	//	return err
 	//}
 
-	// FROST KEYGEN TAPROOT
-	//frostResultTaproot, err := FrostKeygenTaproot(id, ids, threshold, n)
-	//if err != nil {
-	//	return err
-	//}
+	//fmt.Printf("%+v\n", keygenConfig)
 
 	signers := ids[:threshold+1]
 	if !signers.Contains(id) {
@@ -228,36 +93,10 @@ func All(id party.ID, ids party.IDSlice, threshold int, message []byte, n *test.
 
 	// CMP SIGN
 	fmt.Println("cmp sign")
-	err = CMPSign(refreshConfig, message, signers, n, pl)
+	err = CMPSign(keygenConfig, message, signers, n, pl)
 	if err != nil {
 		return err
 	}
-
-	// CMP PRESIGN
-	fmt.Println("cmp presign")
-	preSignature, err := CMPPreSign(refreshConfig, signers, n, pl)
-	if err != nil {
-		return err
-	}
-
-	// CMP PRESIGN ONLINE
-	fmt.Println("cmp presign online")
-	err = CMPPreSignOnline(refreshConfig, preSignature, message, n, pl)
-	if err != nil {
-		return err
-	}
-
-	// FROST SIGN
-	//err = FrostSign(frostResult, id, message, signers, n)
-	//if err != nil {
-	//		return err
-	//}
-
-	// FROST SIGN TAPROOT
-	//err = FrostSignTaproot(frostResultTaproot, id, message, signers, n)
-	//if err != nil {
-	//	return err
-	//}
 
 	return nil
 }
