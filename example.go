cb0098e19c4a7bba1ef3e72ccb7e06a8cba17078
d-smith <doug.smith.mail@gmail.com>
2023-06-17 08:51:45 -0700

    added some presign and sig with presign

--- example.go
+++ example.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"gotss/example/test"
+	"log"
 	"sync"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -73,6 +74,67 @@ func CMPSign(c *cmp.Config, m []byte, signers party.IDSlice, n *test.Network, pl
 	}
 	fmt.Println("public key from sig is", hexutil.Encode(sigPublicKey))
 
+	//------
+
+	sigPublicKeyECDSA, err := crypto.SigToPub(m, sigEth)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sigPublicKeyBytes := crypto.FromECDSAPub(sigPublicKeyECDSA)
+
+	signatureNoRecoverID := sigEth[:len(sigEth)-1] // remove recovery id
+	verified := crypto.VerifySignature(sigPublicKeyBytes, m, signatureNoRecoverID)
+	fmt.Println(verified)
+
+	return nil
+}
+
+func CMPPreSign(c *cmp.Config, signers party.IDSlice, n *test.Network, pl *pool.Pool) (*ecdsa.PreSignature, error) {
+	h, err := protocol.NewMultiHandler(cmp.Presign(c, signers, pl), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	test.HandlerLoop(c.ID, h, n)
+
+	signResult, err := h.Result()
+	if err != nil {
+		return nil, err
+	}
+
+	preSignature := signResult.(*ecdsa.PreSignature)
+	if err = preSignature.Validate(); err != nil {
+		return nil, errors.New("failed to verify cmp presignature")
+	}
+	return preSignature, nil
+}
+
+func CMPPreSignOnline(c *cmp.Config, preSignature *ecdsa.PreSignature, m []byte, n *test.Network, pl *pool.Pool) error {
+	h, err := protocol.NewMultiHandler(cmp.PresignOnline(c, preSignature, m, pl), nil)
+	if err != nil {
+		return err
+	}
+	test.HandlerLoop(c.ID, h, n)
+
+	signResult, err := h.Result()
+	if err != nil {
+		return err
+	}
+	signature := signResult.(*ecdsa.Signature)
+	if !signature.Verify(c.PublicPoint(), m) {
+		return errors.New("failed to verify cmp signature")
+	}
+
+	sigEth, err := signature.SigEthereum()
+	fmt.Println("presign online eth sig from ", c.ID, " is ", hexutil.Encode(sigEth))
+
+	sigPublicKey, err := crypto.Ecrecover(m, sigEth)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("presign online public key from sig is", hexutil.Encode(sigPublicKey))
+
 	return nil
 }
 
@@ -99,6 +161,20 @@ func All(id party.ID, ids party.IDSlice, threshold int, message []byte, n *test.
 	if !signers.Contains(id) {
 		n.Quit(id)
 		return nil
+	}
+
+	// CMP PRESIGN
+	fmt.Println("cmp presign")
+	preSignature, err := CMPPreSign(keygenConfig, signers, n, pl)
+	if err != nil {
+		return nil
+	}
+
+	// CMP PRESIGN ONLINE
+	fmt.Println("cmp presign online")
+	err = CMPPreSignOnline(keygenConfig, preSignature, message, n, pl)
+	if err != nil {
+		return err
 	}
 
 	// CMP SIGN
